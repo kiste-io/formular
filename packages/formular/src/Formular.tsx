73198c41d2455ba77ebab565c772e3aef5bf4bc0
Vsevolod <zavalit@gmail.com>
2021-06-28 14:43:46 +0200

    fix submit

--- packages/formular/src/Formular.tsx
+++ packages/formular/src/Formular.tsx
@@ -1,16 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { FormularData, FormData } from './types'
 
 window.__FORMULAR_DATA__ = {} as FormularData
 
 
+const obtainValues = (elements, formId) => {
+    const formData = window.__FORMULAR_DATA__[formId] || {} as FormData
+    
+    elements.forEach(el => {
+        const {tagName, name, value} = el
+        formData[tagName] = [...(formData[tagName] || []).filter(el => el.name !== name), {name, value}]
+    })
+
+    window.__FORMULAR_DATA__[formId] = formData
+
+}
 
 const changeEventListener = (e, formId) => {
     
-    const data = window.__FORMULAR_DATA__ 
-    const {tagName, name, value} = e.target 
-
-    const formData = data[formId] || {} as FormData
+    const formData = window.__FORMULAR_DATA__[formId] || {} as FormData
+    
+    const {tagName, name, value} = e.target     
     formData[tagName] = [...(formData[tagName] || []).filter(el => el.name !== name), {name, value}]
 
     window.__FORMULAR_DATA__[formId] = formData
@@ -28,32 +38,47 @@ export const Formular = ({children, onSubmit, ...props}) => {
     const fields = React.Children.toArray(children)
 
     const changeFormElementEventListener = (e) => changeEventListener(e, formId)
+    
+    const submitFormListner = (e) => {
+        e.preventDefault();
 
+        const data = window.__FORMULAR_DATA__[formId]
+        console.log('data', data)
+        const payload = Object.values(data)
+        onSubmit(
+            payload
+            .reduce((acc, tokens) => ([...acc, ...tokens]), [])
+            .reduce((acc, token) => ({...acc, [token.name]:token.value}), {})
+            )
+        
+        return false;
+    }
     
     useEffect(() => {
         if(!formRef.current) return
         
-        const submitFormListner = (e) => {
-            e.preventDefault();
-
-            console.log('window.__FORMULAR_DATA__[formId]', window.__FORMULAR_DATA__[formId], window)
-
-            
-            return false;
-        }
-   
-
-        (formRef.current as HTMLFormElement).addEventListener("submit", submitFormListner, true)
-
         const elements = Array.from(formRef.current).filter((el: HTMLBaseElement ) => ['INPUT', 'SELECT', 'TEXTAREA'].includes(el.tagName))
-        
-        setElements(elements as HTMLBaseElement[])
 
-        return () =>  (formRef.current as HTMLFormElement).removeEventListener("submit", submitFormListner)
+        setElements(elements as HTMLBaseElement[])
         
     }, [children])
 
+
     useEffect(() => {
+
+        if(!formRef.current) return
+
+        const form = (formRef.current as HTMLFormElement)
+
+        form.addEventListener("submit", submitFormListner)
+        
+        return () => form.removeEventListener("submit", submitFormListner)
+
+    }, [elements])
+
+    useEffect(() => {
+
+        obtainValues(elements, formId)
 
         elements.forEach(el => el.addEventListener('change', changeFormElementEventListener))
 
